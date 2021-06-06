8beecf148fc11e6b21554d92c51b377b18a4c52e
Naman Sukhwani <namansukhwani@gmail.com>
2021-06-07 02:19:37 +0530

    up

--- next.config.js
+++ next.config.js
@@ -1,25 +1,7 @@
 const withPWA = require('next-pwa')
 const runtimeCaching = require('next-pwa/cache')
 
-module.exports = withPWA({
-    images: {
-        domains: [
-            "dvyvvujm9h0uq.cloudfront.net",
-            "i.pinimg.com",
-            "i.guim.co.uk",
-            "images.unsplash.com",
-            "encrypted-tbn0.gstatic.com",
-            "img.freepik.com",
-            "lh3.googleusercontent.com"
-        ]
-    },
-    pwa: {
-        dest: 'public',
-        runtimeCaching,
-    },
-})
-
-// module.exports = {
+// module.exports = withPWA({
 //     images: {
 //         domains: [
 //             "dvyvvujm9h0uq.cloudfront.net",
@@ -31,4 +13,22 @@ module.exports = withPWA({
 //             "lh3.googleusercontent.com"
 //         ]
 //     },
-// }
+//     pwa: {
+//         dest: 'public',
+//         runtimeCaching,
+//     },
+// })
+
+module.exports = {
+    images: {
+        domains: [
+            "dvyvvujm9h0uq.cloudfront.net",
+            "i.pinimg.com",
+            "i.guim.co.uk",
+            "images.unsplash.com",
+            "encrypted-tbn0.gstatic.com",
+            "img.freepik.com",
+            "lh3.googleusercontent.com"
+        ]
+    },
+}
