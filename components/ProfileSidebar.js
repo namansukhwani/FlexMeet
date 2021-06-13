2bbeb97f5d8b03fcd5463dba287c1ed22aea2536
Naman Sukhwani <namansukhwani@gmail.com>
2021-06-14 00:30:48 +0530

    up

---
+++ components/ProfileSidebar.js
@@ -0,0 +1,23 @@
+import { useRouter } from 'next/router';
+
+const ProfileSidebar = (props) => {
+
+    const router = useRouter();
+
+    //refs
+
+    //states
+
+    //lifecycle
+
+    //methods
+
+    //views
+    return (
+        <div  >
+
+        </div>
+    )
+}
+
+export default ProfileSidebar;
